6209cdf50285d9128eae3f7981a6cd2beef7e1e2
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-10-06 13:18:21 +0200

    sharing state between steps
    that belongs to same step definition class. To be updated

--- src/test/java/awesomecucumber/MyStepDefinitions.java
+++ src/test/java/awesomecucumber/MyStepDefinitions.java
@@ -9,9 +9,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.Select;
 
 import java.util.List;
 import java.util.Map;
@@ -19,6 +17,14 @@ import java.util.Map;
 public class MyStepDefinitions {
 
     private WebDriver driver;
+    private String billingFirstName;
+    private String billingLastName;
+    private String billingAddress1;
+    private String billingCity;
+    private String billingStateName;
+    private String billingZip;
+    private String billingEmail;
+
 
     @Given("I'm on the Store Page")
     public void iMOnTheStorePage() {
@@ -54,17 +60,28 @@ public class MyStepDefinitions {
         new CartPage(driver).checkout();
     }
 
+    @And("my billing details are")
+    public void myBillingDetailsAre(List<Map<String, String>> billingDetails) {
+        billingFirstName = billingDetails.get(0).get("firstname");
+        billingLastName = billingDetails.get(0).get("lastname");
+        billingAddress1 = billingDetails.get(0).get("address_line_1");
+        billingCity = billingDetails.get(0).get("city");
+        billingStateName = billingDetails.get(0).get("state");
+        billingZip = billingDetails.get(0).get("zip");
+        billingEmail = billingDetails.get(0).get("email");
+
+    }
+
     @When("I provide billing details")
-    public void iProvideBillingDetails(List<Map<String, String>> billingDetails) {
+    public void iProvideBillingDetails() {
         CheckoutPage checkoutPage = new CheckoutPage(driver);
-        checkoutPage.setBillingDetails(
-                billingDetails.get(0).get("firstname"),
-                billingDetails.get(0).get("lastname"),
-                billingDetails.get(0).get("address_line_1"),
-                billingDetails.get(0).get("city"),
-                billingDetails.get(0).get("state"),
-                billingDetails.get(0).get("zip"),
-                billingDetails.get(0).get("email"));
+        checkoutPage.setBillingDetails(billingFirstName,
+                billingLastName,
+                billingAddress1,
+                billingCity,
+                billingStateName,
+                billingZip,
+                billingEmail)
     }
 
     @And("I place an order")
